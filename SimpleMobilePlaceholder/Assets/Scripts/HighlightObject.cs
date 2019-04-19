788924a05f641a1da5d0aa2f7a7ebe70824de15b
as75287 <amberrociosmith@gmail.com>
2019-04-19 11:32:16 -0500

    Added highlight script

---
+++ SimpleMobilePlaceholder/Assets/Scripts/HighlightObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighlightObject : MonoBehaviour
+{
+    public Material gazedAtMaterial;
+    private Renderer myRenderer;
+    private Material[] defaultMaterials;
+
+    private void Start()
+    {
+        myRenderer = GetComponent<Renderer>();
+        defaultMaterials = myRenderer.materials;
+    }
+
+    
+    public void SetGazedAt(bool gazedAt)
+    {
+        if (gazedAtMaterial != null)
+        {
+            for (int i = 0; i < defaultMaterials.Length; i++)
+            {
+                myRenderer.materials[i] = gazedAt ? gazedAtMaterial : defaultMaterials[i];
+            }
+        }
+    }
+}
