d0f53d4af72c73075749320a8bf4658a961132cb
Hellorocio <amberrociosmith@gmail.com>
2019-04-21 21:10:31 -0500

    Added scoring and radio turns on and off

---
+++ SimpleMobilePlaceholder/Assets/Scripts/RadioScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadioScript : MonoBehaviour
+{
+    AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void ClickRadio ()
+    {
+        if (audioSource != null)
+        {
+            if (audioSource.isPlaying)
+            {
+                audioSource.Stop();
+            }
+            else
+            {
+                audioSource.Play();
+            }
+        }
+    }
+}
